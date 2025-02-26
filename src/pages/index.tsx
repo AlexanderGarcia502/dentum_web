fc8bad58ffff3003f22ec84c529a7bafd3209956
AlexanderGarcia502 <pablogarica520hotmaill@gmail.com>
2025-02-26 12:40:45 -0600

    Integrate functionality in the index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,13 @@ import "../../public/assets/css/plugins.css";
 import "../../public/assets/css/style.css";
 import "../../public/assets/css/colors/navy.css";
 import Foother from "@/components/foother/Foother";
+import Image from "next/image";
 import { ScriptComponent } from "@/utils/Scripts";
+import { Information } from "@/interfaces/enums/information";
 import { Header } from "@/components/header";
 import { testimonies } from "@/utils/testimonies";
 import { TestimonyCard } from "@/components/card/Testimony";
+import { Button } from "@/components/Button";
 import { services } from "@/utils/services";
 import { ServiceCard } from "@/components/card/ServiceCard";
 import { Location } from "@/components/Location";
@@ -51,12 +54,25 @@ export default function Home() {
                 </h2>
                 <div>
                   <a
-                    href="#"
+                    href={Information.WHATSSAP_CH}
                     className="btn btn-lg btn-navy text-white  border-[#000] hover:text-white hover:bg-[#000] hover:border-[#343f52] focus:shadow-[rgba(82,92,108,1)] active:text-white active:bg-[#343f52] active:border-[#343f52] disabled:text-white disabled:bg-[#343f52] disabled:border-[#343f52]"
                     style={{
                       background: "#000",
                     }}
                   >
+                    <div className="mr-1">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          fill="currentColor"
+                          d="M19.05 4.91A9.82 9.82 0 0 0 12.04 2c-5.46 0-9.91 4.45-9.91 9.91c0 1.75.46 3.45 1.32 4.95L2.05 22l5.25-1.38c1.45.79 3.08 1.21 4.74 1.21c5.46 0 9.91-4.45 9.91-9.91c0-2.65-1.03-5.14-2.9-7.01m-7.01 15.24c-1.48 0-2.93-.4-4.2-1.15l-.3-.18l-3.12.82l.83-3.04l-.2-.31a8.26 8.26 0 0 1-1.26-4.38c0-4.54 3.7-8.24 8.24-8.24c2.2 0 4.27.86 5.82 2.42a8.18 8.18 0 0 1 2.41 5.83c.02 4.54-3.68 8.23-8.22 8.23m4.52-6.16c-.25-.12-1.47-.72-1.69-.81c-.23-.08-.39-.12-.56.12c-.17.25-.64.81-.78.97c-.14.17-.29.19-.54.06c-.25-.12-1.05-.39-1.99-1.23c-.74-.66-1.23-1.47-1.38-1.72c-.14-.25-.02-.38.11-.51c.11-.11.25-.29.37-.43s.17-.25.25-.41c.08-.17.04-.31-.02-.43s-.56-1.34-.76-1.84c-.2-.48-.41-.42-.56-.43h-.48c-.17 0-.43.06-.66.31c-.22.25-.86.85-.86 2.07s.89 2.4 1.01 2.56c.12.17 1.75 2.67 4.23 3.74c.59.26 1.05.41 1.41.52c.59.19 1.13.16 1.56.1c.48-.07 1.47-.6 1.67-1.18c.21-.58.21-1.07.14-1.18s-.22-.16-.47-.28"
+                        />
+                      </svg>
+                    </div>
                     Contáctanos
                   </a>
                 </div>
@@ -113,7 +129,7 @@ export default function Home() {
               </div>
               <div className="mt-12">
                 <a
-                  href="#"
+                  href="/services"
                   className="btn btn-lg btn-navy text-white !bg-[#000] border-[#000] hover:text-white  hover:border-[#000] focus:shadow-[rgba(0,0,0,1)] active:text-white active:bg-[#343f52] active:border-[#343f52] disabled:text-white disabled:bg-[#343f52] disabled:border-[#343f52]"
                   style={{
                     background: "#000",
@@ -178,7 +194,7 @@ export default function Home() {
 
                 <div className="w-full">
                   <a
-                    href="#"
+                    href="/about"
                     className="btn btn-lg btn-navy text-white !bg-[#000] border-[#000] hover:text-white  hover:border-[#000] focus:shadow-[rgba(0,0,0,1)] active:text-white active:bg-[#343f52] active:border-[#343f52] disabled:text-white disabled:bg-[#343f52] disabled:border-[#343f52]"
                     style={{
                       background: "#000",
